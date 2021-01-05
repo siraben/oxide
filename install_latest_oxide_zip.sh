0cf32b61920b723e126bdbdf7c14d6112e966b60
Ben Siraphob <bensiraphob@gmail.com>
2021-01-05 14:06:14 +0700

    Add zip installation script

---
+++ install_latest_oxide_zip.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+set -eux
+
+echo "make sure to have oxide.zip here"
+RM=192.168.0.100
+res=oxide
+unzip oxide.zip -d oxide
+ssh root@$RM "systemctl disable --now tarnish"
+scp -r $res/. root@$RM:/
+ssh root@$RM "systemctl daemon-reload && systemctl disable --now xochitl && systemctl enable --now tarnish"
+rm -rf oxide
